8226c13bd312f83381136520ace02a2a1d3b68ea
Perpo Hipolito <hperpo@gmail.com>
2017-12-19 13:55:10 +0800

    code refactor

--- scripts/pageloadtimer.py
+++ scripts/pageloadtimer.py
@@ -12,92 +12,95 @@
 
 import collections
 import textwrap
-import json
 
 from selenium import webdriver
 
+CHROME_BROWSER_PATH = "/usr/lib/chromium-browser/chromedriver"
+
 class PageLoadTimer:
 
-    def __init__(self, driver):
- 
-        self.driver = driver
+	def __init__(self, driver):
+		self.driver = driver
+		self.jscript = textwrap.dedent("""
+			var performance = window.performance || {};
+			var timings = performance.timing || {};
+			return timings;
+		""")
 
-        self.jscript = textwrap.dedent("""
-            var performance = window.performance || {};
-            var timings = performance.timing || {};
-            return timings;
-            """)
+	def inject_timing_js(self):
 
-    def inject_timing_js(self):
-        timings = self.driver.execute_script(self.jscript)
-        return timings
+		timings = self.driver.execute_script(self.jscript)
 
-    def get_event_times(self):
+		return timings
 
-        timings = self.inject_timing_js()
+	def get_event_times(self):
 
-        min_time = min([epoch for epoch in timings.values() if epoch != 0])
+		timings = self.inject_timing_js()
 
-        ordered_events = ('navigationStart', 'fetchStart', 'domainLookupStart',
-                          'domainLookupEnd', 'connectStart', 'connectEnd','secureConnectionStart',
-                          'requestStart','responseStart', 'responseEnd', 'domLoading',
-                          'domInteractive', 'domContentLoadedEventStart',
-                          'domContentLoadedEventEnd', 'domComplete',
-                          'loadEventStart', 'loadEventEnd'
-                          )
+		min_time = min([epoch for epoch in timings.values() if epoch != 0])
+
+		ordered_events = ('navigationStart', 'fetchStart', 'domainLookupStart',
+			'domainLookupEnd', 'connectStart', 'connectEnd','secureConnectionStart',
+			'requestStart','responseStart', 'responseEnd', 'domLoading',
+			'domInteractive', 'domContentLoadedEventStart',
+			'domContentLoadedEventEnd', 'domComplete',
+			'loadEventStart', 'loadEventEnd'
+			)
+
+
+		results = []
+
+		for event in ordered_events:
+			if event in timings:
+				if timings[event] == 0:
+					results.append((event, timings[event]))
+				if timings[event] > 0:
+					results.append((event, (timings[event] - min_time)))
+		
+		return collections.OrderedDict(results)
+
+
+def calculate_page_time(url):
+
+	results =  {}		
 	
-        results = []
-	
-        for event in ordered_events:
-                if event in timings:
-                        if timings[event] == 0:
-                                results.append((event, timings[event]))
-                        if timings[event] > 0:
-                                results.append((event, (timings[event] - min_time)))
+	driver = None
 
-        return collections.OrderedDict(results)
+	try:
+
+		options = webdriver.ChromeOptions()
+	
+		options.add_argument('headless')
+		options.add_argument('igcognito')
+		options.add_argument('window-size=1920x1080')
+
+		driver = webdriver.Chrome(executable_path=CHROME_BROWSER_PATH,chrome_options=options)
+		driver.implicitly_wait(10)
+
+		driver.get(url)
+		
+		timing = PageLoadTimer(driver)
+		events_time = timing.get_event_times()
+		
+		results["latency"] = events_time["responseStart"] - events_time["fetchStart"]
+		results["transfer"] = events_time["responseEnd"] - events_time["responseStart"]
+		results["dom_processing"] = events_time["domInteractive"] - events_time["domLoading"]
+		results["dom_interactive"] = events_time["domComplete"] - events_time["domInteractive"]
+		results["onload"] = events_time["loadEventEnd"] - events_time["loadEventStart"]
+
+		results["total_page_load"] = results["latency"] + results["transfer"] + results["dom_processing"] + results["dom_interactive"] + results["onload"]				
+			
+	except Exception,e:
+		print e
+	finally:
+		if driver:
+			driver.quit()
+	
+	return results
+	
 
 if __name__ == '__main__':
 
-    url = 'http://buzz4pun.com'
+	url = 'http://facebook.com'
 	
-    options = webdriver.ChromeOptions()
-    #uncomment to run headless
-    options.add_argument('headless')
-    #used igcognito to disable caches
-    options.add_argument('incognito')
-    options.add_argument('window-size=1920x1080')
-    driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver",chrome_options=options)
-    driver.implicitly_wait(10)
-    
-    driver.get(url)
-
-    timer = PageLoadTimer(driver)
-    events_time = timer.get_event_times()
-
-   
-    driver.quit()
-	
-    results = {}
-
-    print "*** Events Time\n"
-    
-    for key in events_time.iterkeys():
-
-    	print "%s: %s" % (key, events_time[key])
-
-    results["latency"] = events_time["responseStart"] - events_time["fetchStart"]
-    results["transfer"] = events_time["responseEnd"] - events_time["responseStart"]
-    results["dom_processing"] = events_time["domInteractive"] - events_time["domLoading"]
-    results["dom_interactive"] = events_time["domComplete"] - events_time["domInteractive"]
-    results["onload"] = events_time["loadEventEnd"] - events_time["loadEventStart"]
-
-    results["total_page_load"] = results["latency"] + results["transfer"] + results["dom_processing"] + results["dom_interactive"] + results["onload"]
-
-
-    print "\n"
-    print "*** Page Load Time Results\n"
-    print json.dumps(results)
-	
-
-	
+	print calculate_page_time(url)
